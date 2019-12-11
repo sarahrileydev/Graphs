5911ff5e39e5dfaf728d63678215b902366b7702
sriley <sarahriley.dev@gmail.com>
2019-12-11 13:14:40 -0500

    add get neighbors

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -30,7 +30,7 @@ class Graph:
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        return self.vertices[vertex_id]
 
     def bft(self, starting_vertex):
         """
